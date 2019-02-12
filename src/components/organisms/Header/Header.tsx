ac6c87bc9ca3621a3a132c428f04ee265f7b9adf
Duy Nguyen <andy@pixeltitanstudios.com>
2019-02-12 11:01:11 -0500

    Created header

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { Badge, Avatar, NameTag } from '../../atoms';
+import background from '../../_global/background.jpg';
+import styles from './Header.module.scss';
 
 const Header = () => (
-    <header>
-        <Badge 
-            prefix='fal'
-            icon='plus'
-        />
+    <header className={styles.container}>
+        <img src={background} alt={'Background'} className={styles.background} />
+        
+        <div style={{ display: 'flex', padding: '0 30px'}}>
+            <Badge 
+                prefix='fal'
+                icon='plus'
+            />
 
-        <Badge 
-            prefix='fal'
-            icon='folder'
-            count={3}
-        />
+            <Badge 
+                prefix='fal'
+                icon='folder'
+                count={3}
+            />
 
-        <Badge 
-            prefix='fal'
-            icon='bell'
-            count={123}
-        />
+            <Badge 
+                prefix='fal'
+                icon='bell'
+                count={123}
+            />
+        </div>
 
         <NameTag title={'Web Designer'} right={true}>{'Devin Townsend'}</NameTag>
         <Avatar url={'https://randomuser.me/api/portraits/men/32.jpg'} />
