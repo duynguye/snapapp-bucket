86441abbb4f177fee5555fb61211986ab86c7a67
Duy Nguyen <andy@pixeltitanstudios.com>
2019-02-12 11:34:47 -0500

    Updated readme

--- src/components/organisms/LocalMenu/LocalMenu.stories.js
+++ src/components/organisms/LocalMenu/LocalMenu.stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react';
 import StoryRouter from 'storybook-react-router';
 import GlobalMenu from '../GlobalMenu/GlobalMenu';
 import LocalMenu from './LocalMenu';
+import { ContextLink } from '../../atoms';
 
 const apps = [
     {
@@ -39,6 +40,16 @@ stories.add('default', () => <LocalMenu />);
 stories.add('with Global Menu', () => (
     <React.Fragment>
         <GlobalMenu apps={apps} />
-        <LocalMenu />
+        <LocalMenu>
+            <ContextLink exact={true} path='/' icon={['fal', 'folder']}>My Workspace</ContextLink>
+            <ContextLink exact={false} path='/projects' icon={['fal', 'rocket']}>Projects</ContextLink>
+            <ContextLink exact={false} path='/proposals' icon={['fal', 'desktop']}>Proposals</ContextLink>
+            <ContextLink exact={false} path='/contracts' icon={['fal', 'file-contract']}>Contracts</ContextLink>
+            <ContextLink exact={false} path='/contests' icon={['fal', 'trophy']}>Contests</ContextLink>
+            <ContextLink exact={false} path='/archives' icon={['fal', 'archive']}>Archives</ContextLink>
+            <ContextLink exact={false} path='/domain' icon={['fal', 'atlas']}>Domain &amp; DNS</ContextLink>
+            <ContextLink exact={false} path='/hosting' icon={['fal', 'server']}>Hosting</ContextLink>
+            <ContextLink exact={false} path='/plugins' icon={['fal', 'plug']}>Plugins</ContextLink>
+        </LocalMenu>
     </React.Fragment>
 ))
--- src/components/organisms/LocalMenu/LocalMenu.tsx
+++ src/components/organisms/LocalMenu/LocalMenu.tsx
@@ -5,37 +5,25 @@ import background from '../../_global/background.jpg';
 import styles from './LocalMenu.module.scss';
 
 interface ILocalMenuProps {
-    onClick: () => {}
+    onClick: () => {};
+    title: string;
+    children: React.ReactNode;
 }
 
-interface ILocalMenuState {
-
-}
-
-class LocalMenu extends Component<ILocalMenuProps, ILocalMenuState> {
-    render() {
-        return (
-            <div className={styles.container}>
-                <div className={styles.header}>
-                    <PageTitleWithButton onClick={this.props.onClick}>Project Manager</PageTitleWithButton>    
-                </div>
-
-                <div className={styles.body}>
-                    <ContextLink exact={true} path='/' icon={['fal', 'folder']}>My Workspace</ContextLink>
-                    <ContextLink exact={false} path='/projects' icon={['fal', 'rocket']}>Projects</ContextLink>
-                    <ContextLink exact={false} path='/proposals' icon={['fal', 'desktop']}>Proposals</ContextLink>
-                    <ContextLink exact={false} path='/contracts' icon={['fal', 'file-contract']}>Contracts</ContextLink>
-                    <ContextLink exact={false} path='/contests' icon={['fal', 'trophy']}>Contests</ContextLink>
-                    <ContextLink exact={false} path='/archives' icon={['fal', 'archive']}>Archives</ContextLink>
-                    <ContextLink exact={false} path='/domain' icon={['fal', 'atlas']}>Domain &amp; DNS</ContextLink>
-                    <ContextLink exact={false} path='/hosting' icon={['fal', 'server']}>Hosting</ContextLink>
-                    <ContextLink exact={false} path='/plugins' icon={['fal', 'plug']}>Plugins</ContextLink>
-                </div>
-
-                <img className={styles.background} src={background} alt="Background Image" />
+const LocalMenu = ({ onClick, title, children }: ILocalMenuProps) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <PageTitleWithButton onClick={onClick}>Project Manager</PageTitleWithButton>    
             </div>
-        );
-    }
+
+            <div className={styles.body}>
+                { children }
+            </div>
+
+            <img className={styles.background} src={background} alt="Background Image" />
+        </div>
+    );
 }
 
 export default LocalMenu;
